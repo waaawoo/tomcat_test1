3fab6079a19e47de25cb733afbcec70dbf83fe34
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-05-12 22:43:48 +0900

    Initial commit

---
+++ src/tomcat_test/HelloWorld.java
@@ -0,0 +1,24 @@
+package tomcat_test;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloWorld extends HttpServlet {
+private static final long serialVersionUID = 1L;
+
+public void doGet(HttpServletRequest request, HttpServletResponse response)
+throws IOException, ServletException
+{
+response.setContentType("text/html");
+PrintWriter out = response.getWriter();
+out.println("<html>");
+out.println("<head>");
+out.println("<title>Hello World!</title>");
+out.println("</head>");
+out.println("<body>");
+out.println("<h1>hennkousimasita"
+		+ "</h1>");
+out.println("</body>");
+out.println("</html>");
+}
+}
