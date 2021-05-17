7571da8e861dbd99b312b0e40a0d2e0e6c866d60
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-05-17 15:23:28 +0900

    セッション操作

--- src/login/LoginServlet.java
+++ src/login/LoginServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LoginServlet extends HttpServlet {
 
@@ -27,9 +28,14 @@ public class LoginServlet extends HttpServlet {
 		}
 		
 //		リクエストでデータを転送する
-		request.setAttribute("id", id);
-		request.setAttribute("pass", pass);
+//		request.setAttribute("id", id);
+//		request.setAttribute("pass", pass);
 		request.setAttribute("ste", message);
+		
+//		 セッションへデータを格納
+		HttpSession session = request.getSession( true );
+		session.setAttribute("id", id);
+		session.setAttribute("pass", pass);
 		
 		
 //		フォワードで転送
